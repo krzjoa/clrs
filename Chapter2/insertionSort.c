1f25b0e0c2d1c8c340f3ac2a92d79e3bd3afc463
Krzysztof <joachimiak@mywhizzy.com>
2017-04-23 22:18:35 +0200

    insertion sort

---
+++ Chapter2/insertionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*Insertion sort*/
+
+
+/*Inplace*/
+
+void insertionSort(int *inputArray, size_t size){
+  int length = (int) size; //sizeof(inputArray) / sizeof();
+
+  // printf("%d\n", length);
+
+  int i, key;
+
+  for
+
+
+
+}
+
+
+int main(){
+
+  int testArray[5] =  {99, 23, 1, 89, 10};
+  insertionSort(testArray, sizeof testArray / sizeof * testArray);
+
+
+}
