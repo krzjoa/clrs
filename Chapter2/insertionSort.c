800ed9323b7add6114e2424c519f94d33fbebdd5
Krzysztof Joachimiak <kjoa.92@gmail.com>
2017-05-07 22:37:39 +0200

    desc insertion sort

--- Chapter2/insertionSort.c
+++ Chapter2/insertionSort.c
@@ -3,16 +3,43 @@
 /*Insertion sort*/
 
 
-/*Inplace*/
+/* Ascending order.
+  Works inplace.
+*/
 
 void insertionSort(int *inputArray, size_t size){
   int length = (int) size; //sizeof(inputArray) / sizeof();
 
-  // printf("%d\n", length);
+  int i, j, key;
 
-  int i, key;
+  for(j=1; j < length; j++){
+    key = inputArray[j];
+    i = j - 1;
+    while (i > -1 && inputArray[i] > key) {
+      inputArray[i + 1] = inputArray[i];
+      i--;
+    }
+    inputArray[i + 1] = key;
+  }
+}
 
-  for
+/*Descending order (excercise 2.1-2)*/
+
+void inversedInserionSort(int *array, size_t size){
+  int len = (int) size;
+  int i, j, key;
+
+  for(j=1; j<len; j++){
+    key = array[j];
+    i = j - 1;
+    while (i > -1 && array[i] < key) {
+      array[i+1]=array[i];
+      i--;
+    }
+    array[i+1] = key;
+
+  }
+
 
 
 
@@ -20,9 +47,14 @@ void insertionSort(int *inputArray, size_t size){
 
 
 int main(){
+  int i;
 
   int testArray[5] =  {99, 23, 1, 89, 10};
-  insertionSort(testArray, sizeof testArray / sizeof * testArray);
+  inversedInserionSort(testArray, sizeof testArray / sizeof * testArray);
+
+  for(i=0; i<5; i++){
+    printf("%d\n", testArray[i]);
+  }
 
 
 }
