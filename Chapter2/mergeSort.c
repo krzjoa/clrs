85b4daa28690bb0a708f12dc45aaa9b1dec1ee31
Krzysztof <joachimiak@mywhizzy.com>
2018-02-01 20:48:16 +0100

    Merge

--- Chapter2/mergeSort.c
+++ Chapter2/mergeSort.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 /* Merge sort*/
 
 
 void merge(int *array, int p, int q, int r){
 
+  // Number of elements in two lists
+  int n1 = q - p + 1;
+  int n2 = r - q;
 
-  int internalArray[5];
+  // New arrays
+  int *L, *R;
+  L = malloc(n1 * sizeof(int));
+  R = malloc(n2 * sizeof(int));
 
-  memcpy(internalArray, array, sizeof(int)*5);
-  internalArray[0] = 543;
-
-  for(int i=0; i<5; i++){
-    printf("%d\n", internalArray[i]);
+  // Filling lists with elements
+  for(int i=0; i<n1; i++){
+      L[i] = array[p+i];
   }
+
+  for(int i=0; i<n2; i++){
+    R[i] = array[q+i];
+  }
+
+  // Assign big values to the last elements of the lists
+  uintmax_t tmp;
+
+  L[n1] = tmp;
+  R[n2] = tmp;
+
+  for(int i=0; i<4; i++){
+    printf("%d\n", L[i]);
+  }
+
+
+  //int internalArray[5];
+
+  // memcpy(internalArray, array, sizeof(int)*5);
+  // internalArray[0] = 543;
+  //
+
 
 
   //printf("%d\n", array[p]);
@@ -29,8 +57,8 @@ void merge(int *array, int p, int q, int r){
 
 int main(){
 
-  int testArray[5] =  {99, 23, 1, 89, 10};
-  merge(testArray, 1, 2, 3);
+  int testArray[10] =  {99, 23, 1, 89, 10, 6, 84, 34, 26, 45};
+  merge(testArray, 0, 2, 4);
 
   for(int i=0; i<5; i++){
     printf("%d\n", testArray[i]);
